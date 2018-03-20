a0898d0172b4c6bab11c2f4bdfd58a323307699a
13568810905 <weiyi@zhengweiyi.cn>
2018-03-20 08:55:01 +0800

    变更 承诺页文件位置

--- pages/poster/promise/promise.js
+++ pages/poster/promise/promise.js
@@ -1,80 +1,66 @@
-// pages/poster/poster.js
+// pages/poster/promise/promise.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    carousels:[
-      { url: 'http://www.epearth.com/testimg/logo.png' },
-      { url: 'http://www.epearth.com/testimg/agan.png' },
-      { url: 'http://www.epearth.com/testimg/logo.png' }
-    ],
-    className:[
-      { string: '手动挡'},
-      { string: '自动挡'}
-    ]
+    img: 'http://www.epearth.com/testimg/promise.jpg'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
-  },
 
-  clickPromise: function () {
-    wx.navigateTo({
-      url: '../poster/promise/promise',
-    })
-  },
+  }
 })
