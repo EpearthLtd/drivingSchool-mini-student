d2893a493c747934d8aae7f37978b767c1838cd6
郑维一 <weiyi@zhengweiyi.cn>
2018-06-26 13:48:50 +0800

    添加 手动档、自动档介绍详情页

--- pages/poster/poster.js
+++ pages/poster/poster.js
@@ -19,8 +19,8 @@ Page({
       { url: 'https://aganxueche.com/testimg/ad3.jpg', bindtap: '' }
     ],
     priceView:[
-      { image: '../../images/car.png', name: '手动档', price: '2680', descriptionL1: '尊享学车优惠　体检考试接送', descriptionL2: '全程售后服务'},
-      { image: '../../images/car.png', name: '自动档', price: '3399', descriptionL1: '尊享学车优惠　体检考试接送', descriptionL2: '全程售后服务' },
+      { image: '../../images/car.png', name: '手动档', price: '2680', descriptionL1: '杜绝吃拿卡要　不满极速退款', descriptionL2: '尊享全程售后　线下消费折扣', bindtap: 'clickManual' },
+      { image: '../../images/car.png', name: '自动档', price: '3399', descriptionL1: '杜绝吃拿卡要　不满极速退款', descriptionL2: '尊享全程售后　线下消费折扣', bindtap: 'clickAutomatic' },
     ]
   },
 
@@ -100,12 +100,31 @@ Page({
     })
   },
 
+/**
+ * 用户点击手动档、自动档
+ */
+  clickManual: function () {
+    console.log('用户点击手动档')
+    wx.navigateTo({
+      url: 'manual/manual',
+    })
+  },
+  clickAutomatic: function () {
+    console.log('用户点击自动档')
+    wx.navigateTo({
+      url: 'automatic/automatic',
+    })
+  },
+
   clickC: function () {
     wx.navigateTo({
       url: '../nopage/nopage',
     })
   },
 
+/**
+ * 服务承诺、学车流程、报名须知、常见问题4个页面按钮
+ */
   clickPromise: function () {
     wx.navigateTo({
       url: '../poster/promise/promise',
